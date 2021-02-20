4ab0925a02f80d9ea16ac464b8adead8ad51118d
zephyrus21 <pandey.piyus@gmail.com>
2021-02-20 19:56:38 +0530

    adding characters code

--- build/index.js
+++ build/index.js
@@ -2,7 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var Sorter_1 = require("./Sorter");
 var NumbersCollection_1 = require("./NumbersCollection");
+var CharactersCollection_1 = require("./CharactersCollection");
 var numbers = new NumbersCollection_1.NumbersCollection([1, 5, 523, 2, -3]);
-var sorter = new Sorter_1.Sorter(numbers);
-sorter.sort();
+var characters = new CharactersCollection_1.CharactersCollection('bca');
+var sorter1 = new Sorter_1.Sorter(numbers);
+var sorter2 = new Sorter_1.Sorter(characters);
+sorter1.sort();
+sorter2.sort();
 console.log(numbers.data);
+console.log(characters.data);
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
 const numbers = new NumbersCollection([1, 5, 523, 2, -3]);
-const sorter = new Sorter(numbers);
+const characters = new CharactersCollection('bca');
+const sorter1 = new Sorter(numbers);
+const sorter2 = new Sorter(characters);
 
-sorter.sort();
+sorter1.sort();
+sorter2.sort();
 
 console.log(numbers.data);
+console.log(characters.data);
