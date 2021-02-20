662ce6745ba6bb44cdf9d8ccd8091e2090e40286
zephyrus21 <pandey.piyus@gmail.com>
2021-02-20 20:38:19 +0530

    adding LL algo

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,28 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 const numbers = new NumbersCollection([1, 5, 523, 2, -3]);
 const characters = new CharactersCollection('bca');
+const linkedList = new LinkedList();
+
+linkedList.add(500);
+linkedList.add(0);
+linkedList.add(21);
+
+linkedList.print();
+console.log(numbers.data);
+console.log(characters.data);
+
 const sorter1 = new Sorter(numbers);
 const sorter2 = new Sorter(characters);
+const sorter3 = new Sorter(linkedList);
 
 sorter1.sort();
 sorter2.sort();
+sorter3.sort();
 
+linkedList.print();
 console.log(numbers.data);
 console.log(characters.data);
